eb173e6ea9cceb5531078ff24ff3c19c553322e7
gavan <gavan.jon.lamb@gmail.com>
2021-08-22 19:01:57 +1000

    Add ClaimsPrincipal.GetSubject extension method

---
+++ src/Expensely.Authentication.Cognito.Jwt/Extensions/ClaimsPrincipal.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq;
+
+namespace Expensely.Authentication.Cognito.Jwt.Extensions
+{
+    public static class ClaimsPrincipal
+    {
+        public static string GetSubject(this System.Security.Claims.ClaimsPrincipal principal) => principal.Claims.FirstOrDefault(x => x.Type == "sub")?.Value;
+    }
+}
